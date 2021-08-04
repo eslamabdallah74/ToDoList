14b67eec6368b095b93979dd13a94f28d2b31042
Eslam Abdallah <eslamabdallah301@gmail.com>
2021-08-04 15:35:09 +0200

    backend

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// Starting Routes
+Route::get('/items', [ItemController::class, 'index']);
+Route::prefix('items')->group(function () {
+    Route::post('/store', [ItemController::class, 'store']);
+    Route::put('/{id}', [ItemController::class, 'update']);
+    Route::delete('/{id}', [ItemController::class, 'destroy']);
+
 });
